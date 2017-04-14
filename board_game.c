f1260f53cc16de404d616331ae584c975590a9bb
ZacharyOConnor <zachary.oconnor@ucdconnect.ie>
2017-04-14 23:09:17 +0100

    Update board_game.c
    adding in a function that will let user input the name and type of each player in the game

--- board_game.c
+++ board_game.c
@@ -8,6 +8,10 @@
 #include<time.h>
 #define MAX_PLAYERS 6
 #include"cross_fire_operations.h"
+#include"move_player.h"
+#include"player_capabilities.h"
+#include"changin_player_capabilities.h"
+#include"slot_assignment.h"
 
 struct Player
 {
@@ -21,48 +25,26 @@ struct Player
 	struct slot *place;
 };
 
-
-
-struct Slot
-{
-	int row;
-	int column;
-	struct slot *left
-	struct slot *right
-	struct slot *up
-	struct slot *down
-};
-struct Slot *top_left;
-struct Slot *top_right;
-struct Slot *bottom_left;
-struct Slot *bottom_right;
-
 int main(void)
 {
-	int row, column;
+	int row, column, i;
 	int board_size = 7;//setting the size of the board.
-	struct Slot *current_slot = NULL;
-	struct Slot *found_slots;
-	bool explored[board_size][board_size]//for checking if a slot contains something?
-	int count = 0;
-	//set up the pointers to the 4 corner slots
-	struct Slot *top_left;
-	struct Slot *top_right;
-	struct Slot *bottom_left;
-	struct Slot *bottom_right;
 
 	//creating the board.
-	create_board(board_size, Slot **top_left, Slot **top_right, Slot **bottom_left, Slot **bottom_right);
-
-
-
-
-
 
 	printf("Please enter the number of players you'd\n");
 	printf("like to enter into the game : \n");
 	int num_of_players;
 	scanf("%d", &num_of_players);
+	struct Player player_array[num_of_players];
+	struct PLayer *pplayer_array[num_of_players];
+	
+	for (i = 0; i < num_of_players; i++)
+	{
+		pplayer_array[num_of_players] = &player_array[num_of_players];
+		printf("Please enter the name and type of player number %d", i + 1);
+		input_player(pplayer_array[i]);
+	}
 }
 
 void create_board(int board_size, struct Slot **up_left, struct Slot **up_right, struct Slot **down_left, struct Slot **down_right)
@@ -117,4 +99,12 @@ void assign_human_capabilities(struct Player *pplayer);
 		   sum=pplayer->luck+(*structname).dexterity+(*structname).smartness+(*structname).magic_skills+(*structname).strength);
 	 }
    (*structname).life_points=100;
+}
+
+void input_player(struct Player *p)
+{
+	printf("Player name : ");
+	scanf("%s", p->name);
+	printf("\nPlayer type : ");
+	scanf("%s", p->player_type);
 }
